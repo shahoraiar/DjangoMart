7107a3121a28fde00945bcbf5c7db1335bda3ce0
shahoraiar <shahoraiar2000@gmail.com>
2023-12-17 09:44:09 +0600

    host all

--- Django_Mart/settings.py
+++ Django_Mart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lv0__#y@ff4l*)i@c$8c_833=cl0ungu)wt1b)mr3eia8o9rp4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
