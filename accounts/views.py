ef4f71a12974f4441f41a002cf2f4b549f8d4aee
shahoraiar <shahoraiar2000@gmail.com>
2023-12-17 09:57:50 +0600

    done

--- accounts/views.py
+++ accounts/views.py
@@ -17,32 +17,38 @@ def register(request) :
 def profile(request) : 
     return render(request , 'accounts/dashboard.html')
 
-def signin(request) : #login
-    if request.method == 'POST' : 
+def signin(request):
+    if request.method == 'POST':
         user_name = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(username = user_name , password = password)
-        # print(user)
-        #ekhon o log in hoi nai 
-        if not request.session.session_key:
-            request.session.create()
-        session_id = request.session.session_key #session id nie aslam
-        try:
-            cart = Cart.objects.get(cart_id=session_id)
-        except Cart.DoesNotExist:
-            cart = None
-        # cart = Cart.objects.get(cart_id = session_id)
-        is_cart_item_exists = CartItem.objects.filter(cart = cart).exists()
-        if is_cart_item_exists : 
-            cart_item = CartItem.objects.filter(cart = cart)
-            for item in cart_item : 
-                item.user = user
-                item.save()
-        # copy past ses , akhn log in kore dao
-        # log in hoe gece with session idr sob item user er cart e
-        login(request , user)
-        return redirect('profile')
-    return render(request , 'accounts/signin.html')
+        user = authenticate(username=user_name, password=password)
+
+        if user is not None:
+            # Login successful
+            if not request.session.session_key:
+                request.session.create()
+            session_id = request.session.session_key
+
+            try:
+                cart = Cart.objects.get(cart_id=session_id)
+            except Cart.DoesNotExist:
+                cart = None
+
+            is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+
+            if is_cart_item_exists:
+                cart_item = CartItem.objects.filter(cart=cart)
+                for item in cart_item:
+                    item.user = user
+                    item.save()
+
+            login(request, user)
+            return redirect('cart')
+        else:
+            # Login failed, redirect to 'register'
+            return redirect('register')  # Make sure to replace 'register' with your actual URL name
+
+    return render(request, 'accounts/signin.html')
 
 def user_logout(request) : 
     logout(request)
